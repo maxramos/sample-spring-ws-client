088e4ba034001bf1ec8ebfc47a922dfab99d8e05
mramos <mramos@virtela.net>
2018-10-14 03:28:06 +0800

    Added security.

--- src/main/java/com/maxaramos/springwstestclient/WsClientConfig.java
+++ src/main/java/com/maxaramos/springwstestclient/WsClientConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import org.springframework.ws.client.core.WebServiceTemplate;
+import org.springframework.ws.client.support.interceptor.ClientInterceptor;
+import org.springframework.ws.soap.security.wss4j2.Wss4jSecurityInterceptor;
 
 @Configuration
 public class WsClientConfig {
@@ -14,6 +16,7 @@ public class WsClientConfig {
 		webServiceTemplate.setMarshaller(jaxb2Marshaller());
 		webServiceTemplate.setUnmarshaller(jaxb2Marshaller());
 		webServiceTemplate.setDefaultUri("http://localhost:8080/services");
+		webServiceTemplate.setInterceptors(new ClientInterceptor[] { securityInterceptor() });
 		return webServiceTemplate;
 	}
 
@@ -22,6 +25,15 @@ public class WsClientConfig {
 		Jaxb2Marshaller jaxb2Marshaller = new Jaxb2Marshaller();
 		jaxb2Marshaller.setContextPaths("com.maxaramos.springwstest.user", "com.maxaramos.springwstest.address");
 		return jaxb2Marshaller;
+	}
+
+	@Bean
+	public Wss4jSecurityInterceptor securityInterceptor() {
+		Wss4jSecurityInterceptor securityInterceptor = new Wss4jSecurityInterceptor();
+		securityInterceptor.setSecurementActions("UsernameToken");
+		securityInterceptor.setSecurementUsername("mramos");
+		securityInterceptor.setSecurementPassword("changeit");
+		return securityInterceptor;
 	}
 
 	@Bean
